895b66df921fba884829c8bfbc5ee604e30b0961
gordon@genesis2622.com <gordon@genesis2622.com>
2025-12-11 23:48:36 +0000

    add minutes between milestones

--- models/star/fct_client_journey.sql
+++ models/star/fct_client_journey.sql
@@ -21,6 +21,12 @@ milestone_timestamps AS (
         MIN(CASE WHEN event_name = 'product_added_to_cart' THEN event_timestamp END) AS product_added_to_cart_at,
         MIN(CASE WHEN event_name = 'checkout_started' THEN event_timestamp END) AS checkout_started_at,
         MIN(CASE WHEN event_name = 'checkout_completed' THEN event_timestamp END) AS checkout_completed_at
+
+        -- Calculate minutes between timestamps
+        DATEDIFF('minute', page_viewed_at, product_added_to_cart_at) AS minutes_page_view_to_cart,
+        DATEDIFF('minute', product_added_to_cart_at, checkout_started_at) AS minutes_cart_to_checkout_start,
+        DATEDIFF('minute', checkout_started_at, checkout_completed_at) AS minutes_checkout_start_to_complete
+
     FROM web_events
     GROUP BY client_id
 ),
