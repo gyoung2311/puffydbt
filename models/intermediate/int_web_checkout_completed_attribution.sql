6bc93f4e47cdee3ec82d7789aae2e6f77a0f2612
gordon@genesis2622.com <gordon@genesis2622.com>
2025-12-11 16:46:18 +0000

    add transaction_id and hint for model dependancy

--- models/intermediate/int_web_checkout_completed_attribution.sql
+++ models/intermediate/int_web_checkout_completed_attribution.sql
@@ -1,3 +1,6 @@
+-- revenue_matches_item_sum_int_web_checkout_completed_attribution_item_revenue__int_web_checkout_completed_order_details__revenue__transaction_id":
+-- depends_on: {{ ref('int_web_checkout_completed_order_details') }}
+
 with 
 
 checkout_completed_events as (
@@ -20,6 +23,8 @@ eligible_sessions as(
         checkout_completed_events.client_id,
         checkout_completed_events.event_timestamp as checkout_completed_at,
         checkout_completed_events.revenue,
+        checkout_completed_events.transaction_id,
+    
 
         client_sessions.session_number,
         client_sessions.session_start_at,
@@ -89,6 +94,13 @@ final as (
         checkout_completed_events.client_id,
         checkout_completed_events.event_timestamp as checkout_completed_at,
         checkout_completed_events.revenue,
+        checkout_completed_events.session_number,
+        checkout_completed_events.transaction_id,
+
+        row_number() over (
+            PARTITION by checkout_completed_events.client_id
+            ORDER BY event_timestamp
+        ) as client_purchase_sequence,
 
         first_touch_utm_source,
         first_touch_utm_medium,
@@ -108,4 +120,3 @@ final as (
 )
 
 select * from final
-where client_id = '1741367456-uz2V75aLolvU'
